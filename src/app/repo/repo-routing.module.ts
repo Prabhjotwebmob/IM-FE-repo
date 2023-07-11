1b49772b474ca4432586719b7c63e8078b632c5b
Prabhjot Singh <prabhjot@webmobsoft.com>
2023-07-11 15:36:10 +0530

    routing changes

--- src/app/repo/repo-routing.module.ts
+++ src/app/repo/repo-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { RepoComponent } from './repo/repo.component';
 
 const routes: Routes = [{
-  path: '',
+  path: '**',
   component: RepoComponent
 }];
 
